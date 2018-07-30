00cf27d51b83d5fd0916ce00be95899cc3bb0e1f
Michael <michaellouisdyer@gmail.com>
2018-07-30 17:56:26 -0600

    started interface

--- src/cluster.py
+++ src/cluster.py
@@ -6,22 +6,26 @@ from sklearn.preprocessing import StandardScaler
 from gmplot import gmplot
 from utils import to_markdown
 
-def get_query(best =  False, id = False):
+def get_query(best =  False, id = False, amenity =  None, input_method = 2):
     ### TODO: clean inputs
-    # latitude = float(input("Latitude: "))
-    # longitude = float(input("Longitude: "))
-    # accommodates = float(input("Accommodates: "))
-    # bedrooms = float(input("Beds: "))
-    # bathrooms = float(input("Baths: "))
-    # amenity = input("Amenity: ")
-    longitude  = -104.996133
-    latitude = 39.716561
-    bedrooms = 1.0
-    bathrooms = 1.0
-    accommodates = 2.0
-    amenity = 'pool'
+    unique_amenities =  list(pd.read_pickle('common_amenities.pkl')[0])
+
+    # longitude  = -104.996133
+    # latitude = 39.716561
+    # bedrooms = 1.0
+    # bathrooms = 1.0
+    # accommodates = 2.0
+    # amenity = 'pool'
     property_amenities = []
 
+    if input_method == 2:
+        latitude = float(input("Latitude: "))
+        longitude = float(input("Longitude: "))
+        accommodates = float(input("Accommodates: "))
+        bedrooms = float(input("Beds: "))
+        bathrooms = float(input("Baths: "))
+        print("Available amenities", unique_amenities)
+        amenity = input("Amenity: ").strip().lower()
     if id:
         amenity_df = get_data(id = True)
         id_property = amenity_df.query('airbnb_property_id == @id')
@@ -29,18 +33,18 @@ def get_query(best =  False, id = False):
         property_amenities.remove('bedrooms')
         property_amenities.remove('bathrooms')
 
-
-
         try:
             longitude = id_property['longitude'].iloc[0]
             latitude = id_property['latitude'].iloc[0]
             bedrooms = id_property['bedrooms'].iloc[0]
+            bathrooms = id_property['bathrooms'].iloc[0]
             accommodates = id_property['accommodates'].iloc[0]
         except IndexError:
             print(f'ID # {id} not found')
             quit()
 
     if best:
+        import pdb; pdb.set_trace()
         return { 'bedrooms': bedrooms, 'bathrooms': bathrooms, 'accommodates': accommodates, 'latitude': latitude, 'longitude': longitude}, property_amenities
 
     else:
@@ -89,7 +93,6 @@ def cluster(query, amenity_df, k =  5, verbose = False, distance_weight = 1):
 
 
 def plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, name):
-    # gmap = gmplot.GoogleMapPlotter.from_geocode("Denver")
     gmap = gmplot.GoogleMapPlotter(query_df['latitude'], query_df['longitude'], 13)
     gmap.scatter(w_out_neighbors_df['latitude'], w_out_neighbors_df['longitude'], 'red', size=40, marker=True, title = 'No' + name)
     gmap.scatter(w_neighbors_df['latitude'], w_neighbors_df['longitude'], 'blue', size=40, marker=True, title ='With' + name)
@@ -97,9 +100,9 @@ def plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, name):
     filename = name + '.html'
     gmap.draw(filename)
 
-def best_amenities(id = None, unique = False):
+def best_amenities(input_method, id = None, unique = False):
     amenity_df = get_data()
-    query, property_amenities = get_query(best = True, id = '6333040')
+    query, property_amenities = get_query(best = True, id = id, input_method = input_method)
     unique_amenities =  list(pd.read_pickle('common_amenities.pkl')[0])
     revenue_potentials = pd.DataFrame(columns =['revenue_potential'])
     for amenity in unique_amenities:
@@ -110,16 +113,16 @@ def best_amenities(id = None, unique = False):
         return revenue_potentials.sort_values(by = 'revenue_potential').drop(property_amenities)
     return revenue_potentials.sort_values(by = 'revenue_potential')
 
-def one_amenity():
+def one_amenity(id = False):
     amenity_df = get_data()
-    query = get_query()
+    query = get_query(id)
     amenity_x = query['amenity']
     w_out_neighbors_df, w_neighbors_df, query_df, rev_pot = cluster(query, amenity_df, k=10)
     plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, amenity_x)
 
 
-rev_potentials = best_amenities(id = '6333040', unique = False)
-to_markdown(rev_potentials)
+# rev_potentials = best_amenities(id = '6333040', unique = False)
+# to_markdown(rev_potentials)
 
 def test_weights():
     amenity_df = get_data()
@@ -129,3 +132,25 @@ def test_weights():
         w_out_neighbors_df, w_neighbors_df, query_df, rev_pot = cluster(query, amenity_df, k=10, distance_weight = weight)
         plot_name = amenity_x + f"distance: {weight}"
         plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, plot_name)
+
+
+def main():
+    program = int(input('Select Program: \n 1. Find best amenities \n 2. Select amenity \n'))
+    input_method = int(input('Input information mode: \n 1. Airbnb property ID \n 2. Manual entry\n'))
+    if input_method == 1:
+        print('hi')
+        id = input("ID: ").strip()
+        if program ==1:
+            rev_potentials = best_amenities(input_method = input_method, id = id, unique = False)
+            to_markdown(rev_potentials)
+            pass
+        if program ==2:
+            one_amenity(id)
+            pass
+    if input_method == 2:
+        if program ==1:
+            pass
+        if program ==2:
+            pass
+if __name__ == main():
+    main()
