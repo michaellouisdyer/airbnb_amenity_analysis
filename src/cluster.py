c4575d3df1fbbde0b236e1e722ff95e8cd7548f4
Michael <michaellouisdyer@gmail.com>
2018-07-30 16:54:42 -0600

    added unique amenity features

--- src/cluster.py
+++ src/cluster.py
@@ -20,31 +20,37 @@ def get_query(best =  False, id = False):
     bathrooms = 1.0
     accommodates = 2.0
     amenity = 'pool'
+    property_amenities = []
 
     if id:
         amenity_df = get_data(id = True)
         id_property = amenity_df.query('airbnb_property_id == @id')
-
-        longitude = id_property['longitude'].iloc[0]
-        latitude = id_property['latitude'].iloc[0]
-        bedrooms = id_property['bedrooms'].iloc[0]
-        accommodates = id_property['accommodates'].iloc[0]
+        property_amenities = [x for x in id_property.columns[(id_property == True).all()]]
+        property_amenities.remove('bedrooms')
+        property_amenities.remove('bathrooms')
 
 
+
+        try:
+            longitude = id_property['longitude'].iloc[0]
+            latitude = id_property['latitude'].iloc[0]
+            bedrooms = id_property['bedrooms'].iloc[0]
+            accommodates = id_property['accommodates'].iloc[0]
+        except IndexError:
+            print(f'ID # {id} not found')
+            quit()
 
     if best:
-        return { 'bedrooms': bedrooms, 'bathrooms': bathrooms, 'accommodates': accommodates, 'latitude': latitude, 'longitude': longitude}
+        return { 'bedrooms': bedrooms, 'bathrooms': bathrooms, 'accommodates': accommodates, 'latitude': latitude, 'longitude': longitude}, property_amenities
 
     else:
         return { 'bedrooms': bedrooms, 'bathrooms': bathrooms, 'accommodates': accommodates, 'latitude': latitude, 'longitude': longitude,'amenity': amenity}
-
 
 def cluster(query, amenity_df, k =  5, verbose = False, distance_weight = 1):
     amenity_x = query['amenity']
     scaler =  StandardScaler()
 
     query_df = pd.DataFrame.from_dict(query, orient = 'index').drop('amenity').T
-
 
     df  = amenity_df[['c_revenue_native_ltm', 'bedrooms', 'bathrooms', 'accommodates', 'latitude', 'longitude', amenity_x]]
     w_amenity = df[df[amenity_x]].drop(columns = [amenity_x])
@@ -74,16 +80,12 @@ def cluster(query, amenity_df, k =  5, verbose = False, distance_weight = 1):
     w_out_distance, w_out_neighbors = kn_w_out.kneighbors(w_out_predict)
     w_out_neighbors_df = w_out_amenity.iloc[w_out_neighbors.flatten()]
 
-
-
     w_revenue  = kn_w.predict(w_predict)[0]
     w_out_revenue = kn_w_out.predict(w_out_predict)[0]
     rev_pot = w_revenue -  w_out_revenue
     if verbose:
         print(f'Average yearly revenue with {amenity_x}: $ {w_revenue:.0f} \n Without: $ {w_out_revenue:.0f} \n Yearly revenue potential :  $ { rev_pot:.0f}')
-
     return w_out_neighbors_df, w_neighbors_df, query_df, rev_pot
-
 
 
 def plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, name):
@@ -95,15 +97,17 @@ def plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, name):
     filename = name + '.html'
     gmap.draw(filename)
 
-def best_amenities():
+def best_amenities(id = None, unique = False):
     amenity_df = get_data()
-    query = get_query(best = True, id = '10709107')
+    query, property_amenities = get_query(best = True, id = '6333040')
     unique_amenities =  list(pd.read_pickle('common_amenities.pkl')[0])
     revenue_potentials = pd.DataFrame(columns =['revenue_potential'])
     for amenity in unique_amenities:
         query['amenity'] =  amenity
         w_out_neighbors_df, w_neighbors_df, query_df, rev_pot = cluster(query, amenity_df, k=20)
         revenue_potentials.loc[amenity] = rev_pot
+    if unique:
+        return revenue_potentials.sort_values(by = 'revenue_potential').drop(property_amenities)
     return revenue_potentials.sort_values(by = 'revenue_potential')
 
 def one_amenity():
@@ -113,21 +117,8 @@ def one_amenity():
     w_out_neighbors_df, w_neighbors_df, query_df, rev_pot = cluster(query, amenity_df, k=10)
     plot_neighbors(w_out_neighbors_df, w_neighbors_df, query_df, amenity_x)
 
-def get_query_from_id(id, best = False):
-    amenity_df = get_data(id = True)
-    id_property = amenity_df.query('airbnb_property_id == @id')
 
-
-    if best:
-        return {'bedrooms': id_property['bedrooms'], 'bathrooms': id_property['bathrooms'], 'accommodates': id_property['accommodates'], 'latitude': id_property['latitude'], 'longitude': id_property['longitude']}
-
-    else:
-        amenity = input('Amenity')
-        return {'bedrooms': id_property['bedrooms'], 'bathrooms': id_property['bathrooms'], 'accommodates': id_property['accommodates'], 'latitude': id_property['latitude'], 'longitude': id_property['longitude'], 'amenity': amenity}
-
-    import pdb; pdb.set_trace()
-
-rev_potentials = best_amenities()
+rev_potentials = best_amenities(id = '6333040', unique = False)
 to_markdown(rev_potentials)
 
 def test_weights():
